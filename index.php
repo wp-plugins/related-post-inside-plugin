6e00e5829640c8af295c59ab457b7b40520f4609
Litonice13 <Litonice13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-14 15:28:38 +0000

    Plugin Updated
    git-svn-id: https://plugins.svn.wordpress.org/related-post-inside-plugin/trunk@1067929 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 /*
 Plugin Name: Related Post Inside
-Plugin URI: http://www.h2cweb.net/
+Plugin URI: http://jeweltheme.com/product/related-post-inside-plugin/
 Description: Related Post Inside plugin allows you to insert related posts inside of Posts. Related Post Inside plugin will make your website more SEO friendly, increase post views dramatically. Makes your blogging career more useful and very fast blogging.
 Author: Md. Liton Arefin
-Author URI: http://www.h2cweb.net
-Version: 1.0
+Author URI: http://www.jeweltheme.com
+Version: 1.0.1
+License: GPL2
+http://www.gnu.org/licenses/gpl-2.0.html
 
 /*  Copyright 2013  Liton Arefin  (email : litonice09@gmail.com)
 
@@ -20,23 +22,23 @@ Version: 1.0
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., A product of H2CWEB.NET, Dhaka, Bangladesh
+    Foundation, Inc., A product of jeweltheme.com, Dhaka, Bangladesh
 
 http://www.gnu.org/licenses/gpl.html
 
 */
 
 // Add a menu for our option page
-add_action('admin_menu', 'h2cweb_rpi_add_menu');
+add_action('admin_menu', 'jeweltheme_rpi_add_menu');
 
-function h2cweb_rpi_add_menu() {
+function jeweltheme_rpi_add_menu() {
 
-    add_options_page( 'H2CWEB Related Post Inside', 'H2CWEB RPI', 'manage_options', 'h2cweb_rpi', 'h2cweb_rpi_option_page');
+    add_options_page( 'Jewel Theme Related Post Inside', 'JewelTheme RPI', 'manage_options', 'jeweltheme_rpi', 'jeweltheme_rpi_option_page');
 
 }
 
 //Options menu page
-function h2cweb_rpi_option_page() {
+function jeweltheme_rpi_option_page() {
 ?>
     <div class="wrap">
 
@@ -46,9 +48,9 @@ function h2cweb_rpi_option_page() {
 
         <form action="options.php" method="post">
 
-            <?php settings_fields('h2cweb_rpi_options'); ?>
+            <?php settings_fields('jeweltheme_rpi_options'); ?>
 
-            <?php do_settings_sections('h2cweb_rpi'); ?>
+            <?php do_settings_sections('jeweltheme_rpi'); ?>
 
             <input name="Submit" class="button button-primary" type="submit" value="Save Changes" />
 
@@ -62,67 +64,67 @@ function h2cweb_rpi_option_page() {
 
 
 // Register and define the settings
-add_action('admin_init', 'h2cweb_rpi_admin_init');
+add_action('admin_init', 'jeweltheme_rpi_admin_init');
 
-function h2cweb_rpi_admin_init()
+function jeweltheme_rpi_admin_init()
 
 {
 
-    register_setting( 'h2cweb_rpi_options', 'h2cweb_rpi_options', 'h2cweb_rpi_validate_options' );
+    register_setting( 'jeweltheme_rpi_options', 'jeweltheme_rpi_options', 'jeweltheme_rpi_validate_options' );
 
-    add_settings_section( 'h2cweb_rpi_main', 'Related Post Inside Settings', 'h2cweb_rpi_section_text', 'h2cweb_rpi' );
+    add_settings_section( 'jeweltheme_rpi_main', 'Related Post Inside Settings', 'jeweltheme_rpi_section_text', 'jeweltheme_rpi' );
 
-    add_settings_field( 'h2cweb_rpi_title', 'Related Post Inside Title', 'h2cweb_rpi_setting_title', 'h2cweb_rpi', 'h2cweb_rpi_main' );
+    add_settings_field( 'jeweltheme_rpi_title', 'Related Post Inside Title', 'jeweltheme_rpi_setting_title', 'jeweltheme_rpi', 'jeweltheme_rpi_main' );
 
-    add_settings_field( 'h2cweb_rpi_text_string', 'Number of posts to show', 'h2cweb_rpi_setting_input', 'h2cweb_rpi', 'h2cweb_rpi_main' );    
+    add_settings_field( 'jeweltheme_rpi_text_string', 'Number of posts to show', 'jeweltheme_rpi_setting_input', 'jeweltheme_rpi', 'jeweltheme_rpi_main' );    
 
-    add_settings_field( 'h2cweb_rpi_order', 'Order', 'h2cweb_rpi_order_setting', 'h2cweb_rpi', 'h2cweb_rpi_main' );
+    add_settings_field( 'jeweltheme_rpi_order', 'Order', 'jeweltheme_rpi_order_setting', 'jeweltheme_rpi', 'jeweltheme_rpi_main' );
 
-    add_settings_field( 'h2cweb_rpi_orderby', 'Order By', 'h2cweb_rpi_orderby_setting', 'h2cweb_rpi', 'h2cweb_rpi_main' );
+    add_settings_field( 'jeweltheme_rpi_orderby', 'Order By', 'jeweltheme_rpi_orderby_setting', 'jeweltheme_rpi', 'jeweltheme_rpi_main' );
 
 }
 
 
 // Draw the section header
-function h2cweb_rpi_section_text() {
+function jeweltheme_rpi_section_text() {
 }
 
 // Display and fill the form field
-function h2cweb_rpi_setting_title() {
+function jeweltheme_rpi_setting_title() {
 
-        // get option 'h2cweb_rpi_title' value from the database
-        $options = get_option( 'h2cweb_rpi_options' );
+        // get option 'jeweltheme_rpi_title' value from the database
+        $options = get_option( 'jeweltheme_rpi_options' );
 
-        $h2cweb_rpi_title = $options['h2cweb_rpi_title'];
+        $jeweltheme_rpi_title = $options['jeweltheme_rpi_title'];
 
         // echo the field
-        echo "<input type='text' id='h2cweb_rpi_title' style='width:200px;' name='h2cweb_rpi_options[h2cweb_rpi_title]' value='$h2cweb_rpi_title' />";
+        echo "<input type='text' id='jeweltheme_rpi_title' style='width:200px;' name='jeweltheme_rpi_options[jeweltheme_rpi_title]' value='$jeweltheme_rpi_title' />";
 
 }
 
 // Display and fill the form field
-function h2cweb_rpi_setting_input() {
+function jeweltheme_rpi_setting_input() {
 
         // get option 'rpi_count' value from the database
-        $options = get_option( 'h2cweb_rpi_options' );
+        $options = get_option( 'jeweltheme_rpi_options' );
 
         $rpi_count = $options['rpi_count'];
 
         // echo the field
-        echo "<input type='number' style='width:200px;' id='rpi_count' name='h2cweb_rpi_options[rpi_count]' value='$rpi_count' />";
+        echo "<input type='number' style='width:200px;' id='rpi_count' name='jeweltheme_rpi_options[rpi_count]' value='$rpi_count' />";
 }
   
 //Query Order Posts  
-function h2cweb_rpi_order_setting() {
+function jeweltheme_rpi_order_setting() {
 
         // get option 'rpi_order' value from the database
-        $options = get_option( 'h2cweb_rpi_options' );
+        $options = get_option( 'jeweltheme_rpi_options' );
 
         $rpi_order = $options['rpi_order'];
     
         $items = array("ASC","DESC");
 
-        echo "<select id='rpi_order' name='h2cweb_rpi_options[rpi_order]' style='width:200px;'>";
+        echo "<select id='rpi_order' name='jeweltheme_rpi_options[rpi_order]' style='width:200px;'>";
 
         foreach($items as $item) {
 
@@ -136,16 +138,16 @@ function h2cweb_rpi_order_setting() {
 }
 
 //Query Posts Orderby
-function h2cweb_rpi_orderby_setting() {
+function jeweltheme_rpi_orderby_setting() {
 
         // get option 'rpi_orderby' value from the database
-        $options = get_option( 'h2cweb_rpi_options' );
+        $options = get_option( 'jeweltheme_rpi_options' );
 
         $rpi_orderby = $options['rpi_orderby'];
     
         $items = array("ID","title","date","modified","author","post_name","rand");
 
-        echo "<select id='rpi_orderby' name='h2cweb_rpi_options[rpi_orderby]' style='width:200px;'>";
+        echo "<select id='rpi_orderby' name='jeweltheme_rpi_options[rpi_orderby]' style='width:200px;'>";
 
         foreach($items as $item) {
 
@@ -159,7 +161,7 @@ function h2cweb_rpi_orderby_setting() {
 }
     
 // Validate user input (we want text only)
-function h2cweb_rpi_validate_options( $input ) {
+function jeweltheme_rpi_validate_options( $input ) {
 
         $valid = array();
 
@@ -167,7 +169,7 @@ function h2cweb_rpi_validate_options( $input ) {
 
         $valid['rpi_order'] = preg_replace('/[^a-zA-Z]/', '', $input['rpi_order'] );
 
-        $valid['h2cweb_rpi_title'] = preg_replace('/[^a-z A-Z]/', '', $input['h2cweb_rpi_title'] );
+        $valid['jeweltheme_rpi_title'] = preg_replace('/[^a-z A-Z]/', '', $input['jeweltheme_rpi_title'] );
 
         $valid['rpi_orderby'] = preg_replace('/[^a-zA-Z]/', '', $input['rpi_orderby'] );
 
@@ -178,11 +180,11 @@ function h2cweb_rpi_validate_options( $input ) {
 
 
 /* related posts by category */
-function h2cweb_related_posts_shortcode($atts){
+function jeweltheme_related_posts_shortcode($atts){
 
-    $options = get_option( 'h2cweb_rpi_options' );
+    $options = get_option( 'jeweltheme_rpi_options' );
 
-    $h2cweb_rpi_title = $options['h2cweb_rpi_title'];
+    $jeweltheme_rpi_title = $options['jeweltheme_rpi_title'];
 
     $rpi_count = $options['rpi_count'];
 
@@ -261,11 +263,11 @@ function h2cweb_related_posts_shortcode($atts){
 
     foreach($dtwd_related_posts as $dtwd_related) {
 
-        $options = get_option( 'h2cweb_rpi_options' );
+        $options = get_option( 'jeweltheme_rpi_options' );
 
-        $h2cweb_rpi_title = $options['h2cweb_rpi_title'];
+        $jeweltheme_rpi_title = $options['jeweltheme_rpi_title'];
 
-        $title=$h2cweb_rpi_title ;
+        $title=$jeweltheme_rpi_title ;
 
         $post_list .= '<li><a href="' . get_permalink($dtwd_related->ID) . '">' . $dtwd_related->post_title . '</a></li>';
 
@@ -273,7 +275,7 @@ function h2cweb_related_posts_shortcode($atts){
 
         return sprintf('
 
-            <div class="h2cweb_related-posts">
+            <div class="jeweltheme_related-posts">
 
                 <h4>%s</h4>
 
@@ -285,6 +287,4 @@ function h2cweb_related_posts_shortcode($atts){
 
         }
 
-add_shortcode('rpi', 'h2cweb_related_posts_shortcode');
-
-?>
+add_shortcode('rpi', 'jeweltheme_related_posts_shortcode');
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Related Post Inside Plugin ===
-Contributors: litonice13
+Contributors: litonice13, jwthemeltd
 Donate link: http://liton-online.com/donate
 Tags: WordPress, WordPress Posts, WordPress Related Posts, Inside Related Posts, Related Posts Inside
-Requires at least: 3.0
-Tested up to: 3.5.2
-Stable tag: 1.0
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 1.0.1
 
 	
 Related Post Inside plugin allows you to insert related posts inside of Posts. Related Post Inside plugin will make your website more SEO friendly, increase post views dramatically. Makes your blogging career more useful and very fast blogging.
@@ -12,7 +12,7 @@ Related Post Inside plugin allows you to insert related posts inside of Posts. R
 == Description ==
 Need some extra configurations for Related Post Inside Plugin (if you want it more useful for you). 
 Related Post Inside Plugin allows to change the "Related Post" Title, Number of posts to show, order and Order By. 
-With the activation of Related Post Inside a settings section named "H2CWEB RPI" will be created. For use this plugin a shortcode [rpi] will be needed.
+With the activation of Related Post Inside a settings section named "Jewel Theme RPI" will be created. For use this plugin a shortcode [rpi] will be needed.
 
 = Features Included =
 * Related Post Inside Title.
@@ -22,13 +22,13 @@ With the activation of Related Post Inside a settings section named "H2CWEB RPI"
 * Many more features including Editor Buttons is coming.
 
 = About the Author =
-A product by H2CWEB.NET [WordPress Team](http://h2cweb.net).
+A product by Jewel Theme [WordPress Team](http://jeweltheme.com).
 
 = Problems and Support =
-To get faster response You can drop a mail in our website [Contact Page](http://h2cweb.net/contact-us/). If you have any query about Related Post Inside Plugin then please check our [F.A.Q](http://wordpress.org/plugins/related-post-inside-plugin/faq). 
+To get faster response You can create a Topics our Website  [Support Forum](http://jeweltheme.com/support/forum/wordpress-plugins/related-post-inside). If you have any query about Related Post Inside Plugin then please check our [F.A.Q](http://wordpress.org/plugins/related-post-inside-plugin/faq). 
 
 = Comments, Feedback and Request Features =
-Please give your valueable suggestions, comments, and feedbacks about this plugin or, any ideas about WordPress Plugin just drop a [message to us](http://h2cweb.net/contact-us). We are waiting for hearing from you!!!
+Please give your valueable suggestions, comments, and feedbacks about this plugin or, any ideas about WordPress Plugin just drop a [Support Forum](http://jeweltheme.com/support/forum/wordpress-plugins/related-post-inside). We are waiting for hearing from you!!!
 
 
 This plugin is licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
@@ -39,7 +39,7 @@ This plugin is licensed under the [Apache License, Version 2.0](http://www.apach
 *  From WordPress Dashboard go to "Plugins>> Add New>> Uploads", select 'related-post-inside.zip' file and upload it. Or,
 *  Extract the zip file, copy and paste to the 'wp-conten/plugins/' directory of your WordPress Installation. 
 *  Activate the plugin.
-*  See a Settings Page under Settings section named it "H2CWEB RPI" is created. In the Editor place this shortcode [rpi]. It is btter to place this shortcode in the Editor "Text" mode. 
+*  See a Settings Page under Settings section named it "Jewel Theme RPI" is created. In the Editor place this shortcode [rpi]. It is btter to place this shortcode in the Editor "Text" mode. 
 
 
 == Screenshots ==
@@ -50,7 +50,11 @@ This plugin is licensed under the [Apache License, Version 2.0](http://www.apach
 
 == Changelog ==
 
-= 1.00 =
+= 1.0.1 =
+* Version updated
+* Fixed override contents
+
+= 1.0.0 =
 * Initial Release
 
 
